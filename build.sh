a374189ad3769cbffe6b63453101b30bd2087eb5
Edoardo Marangoni <ecmm@anche.no>
2025-01-20 15:21:57 +0100

    test, add patches

--- build.sh
+++ build.sh
@@ -34,22 +34,26 @@ fi
 
 cd v8
 
-gn gen out/release --args="is_component_build=false  \
-  v8_static_library=true \
-  is_clang=false  \
-  is_asan=false \
-  is_debug=false \
+for patch in ../patches/*.patch; do 
+  git am $patch
+done 
+
+gn gen out/x64.debug --args="is_debug=false \
+  v8_symbol_level=2 \
+  is_component_build=false \
   is_official_build=false \
-  v8_enable_sandbox=false \
-  treat_warnings_as_errors=false \
-  clang_use_chrome_plugins=false \
-  v8_enable_i18n_support=false \
-  v8_use_external_startup_data=false \
   use_custom_libcxx=false \
+  use_custom_libcxx_for_host=true \
   use_sysroot=false \
-  v8_enable_slow_dchecks=false \
-  v8_dcheck_always_on=false \
-  v8_enable_fast_mksnapshot=true \
+  use_glib=false \
+  is_clang=false \
+  v8_expose_symbols=true \
+  v8_optimized_debug=false \
+  v8_enable_sandbox=false \
+  v8_enable_i18n_support=false \
+  v8_enable_gdbjit=false \
+  v8_use_external_startup_data=false \
+  treat_warnings_as_errors=false \
   target_cpu=\"$ARCH\""
 
 # Showtime!
