f20493fe3d6864d6beb1ab4041b6e56a45432b70
Edoardo Marangoni <ecmm@anche.no>
2025-03-12 17:15:09 +0100

    fix(msvc): Try echoing the config to the gn config file

--- build.ps1
+++ build.ps1
@@ -29,8 +29,12 @@ foreach ($f in $files){
   git apply --ignore-space-change --ignore-whitespace $f
 }
 
-gn gen out/release --args="is_debug=false v8_symbol_level=2 is_component_build=false is_official_build=false use_custom_libcxx=false use_custom_libcxx_for_host=true use_sysroot=false use_glib=false is_clang=false v8_expose_symbols=true v8_optimized_debug=false v8_enable_sandbox=false v8_enable_i18n_support=false v8_enable_gdbjit=false v8_use_external_startup_data=false 
-  treat_warnings_as_errors=false target_cpu=\"$env:ARCH\" v8_target_cpu=\"$env:ARCH\" target_os=\"$env:OS\""
+
+New-Item -ItemType Directory -Force -Path .\out\release
+
+echo "is_debug=false v8_symbol_level=2 is_component_build=false is_official_build=false use_custom_libcxx=false use_custom_libcxx_for_host=true use_sysroot=false use_glib=false is_clang=false v8_expose_symbols=true v8_optimized_debug=false v8_enable_sandbox=false v8_enable_i18n_support=false v8_enable_gdbjit=false v8_use_external_startup_data=false treat_warnings_as_errors=false target_cpu=\"$env:ARCH\" v8_target_cpu=\"$env:ARCH\" target_os=\"$env:OS\""| Out-File .\out\release 
+  
+gn gen out/release 
 
 # Showtime!
 ninja -C out/release wee8
