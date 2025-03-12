c528442310fd20ccd840c5f0b1542a54782f0c11
Edoardo Marangoni <ecmm@anche.no>
2025-03-12 19:12:17 +0100

    Remove all `-Wno-*` flags in msvc

--- build.ps1
+++ build.ps1
@@ -35,7 +35,7 @@ New-Item -ItemType Directory -Force -Path .\out\release
 echo "is_debug=false v8_symbol_level=2 is_component_build=false is_official_build=false use_custom_libcxx=false use_custom_libcxx_for_host=true use_sysroot=false use_glib=false is_clang=false v8_expose_symbols=true v8_optimized_debug=false v8_enable_sandbox=false v8_enable_i18n_support=false v8_enable_gdbjit=false v8_use_external_startup_data=false treat_warnings_as_errors=false target_cpu=`"$env:ARCH`" v8_target_cpu=`"$env:ARCH`" target_os=`"win`""| Out-File .\out\release\args.gn
  
 
-Set-Content -Path ".\BUILD.gn" -Value (get-content -Path ".\BUILD.gn" | Select-String -Pattern '"-Wno-invalid-offsetof",' -NotMatch)
+Set-Content -Path ".\BUILD.gn" -Value (get-content -Path ".\BUILD.gn" | Select-String -Pattern '"-Wno.*",' -NotMatch)
 
 gn gen out/release 
 
